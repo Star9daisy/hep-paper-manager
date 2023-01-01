931e3cb87c322da6e60775b26a1e79a26cb95593
Star9daisy <star9daisy@outlook.com>
2023-01-01 20:48:25 +0800

    perf: Update all functions

--- paper_adder.py
+++ paper_adder.py
@@ -1,161 +1,33 @@
-import json
-
-import requests
-from rich import print
-
-from utils import Paper, to_property, to_relation
 import typer
+from rich import print
+from utils import search_in_semantic, search_in_inspire, add_to_notion
 
 
-def main(
-    corpus_id: str,
-    topic: str = "Unkonwn",
-    review: bool = False,
-    source: str = "remote",
-    dry_run: bool = False,
-):
-    # interface
-    # ---------------------------------------------------------------------------- #
-    paper = Paper()
+# test data
+# 251018640: no publication_info
+# 246652443: publication_info has no journal_title
+# 254591719: conference paper
+# 67855972: regular paper
 
-    # semantic engine
-    # ---------------------------------------------------------------------------- #
-    semantic_url = "https://api.semanticscholar.org/graph/v1/paper"
-    # ! tldr service may have Internet Server Error in some cases
-    # check it with corpus_id 254636328
-    # so I remove it temporarily
-    fields = "title,authors,citationCount,journal,externalIds,url"
-    semantic_url = f"{semantic_url}/CorpusID:{corpus_id}?fields={fields}"
-    semantic_response = requests.get(semantic_url)
-    semantic_content = json.loads(semantic_response.text)
+app = typer.Typer()
 
-    # add paper info
-    paper.title = semantic_content["title"] if not review else f"✧ {semantic_content['title']}"
-    paper.authors = [i["name"] for i in semantic_content["authors"][:10]]
-    paper.citations = semantic_content["citationCount"]
-    if semantic_content["journal"]:
-        paper.published = semantic_content["journal"]["name"]
-        if paper.published == "":
-            paper.published = "Unpublished"
 
-    # paper.tldr = semantic_content["tldr"]["text"] if semantic_content["tldr"] else "No tldr yet"
-    paper.source = "Semantic"
-    paper.topic = topic
-    paper.corpus_id = str(semantic_content["externalIds"]["CorpusId"])
-    paper.arxiv_id = semantic_content["externalIds"].get("ArXiv", "None")
-    paper.semantic_link = semantic_content["url"]
+@app.command()
+def main(corpus_id: str, topic: str = "Unknown"):
+    print(f"> Search paper {corpus_id} in Semantic Scholar...")
+    semantic_paper = search_in_semantic(corpus_id)
+    print(semantic_paper)
 
-    # inspire
-    # ---------------------------------------------------------------------------- #
-    inspire_url = "https://inspirehep.net/api"
-    inspire_url = f"{inspire_url}/arxiv/{paper.arxiv_id}"
-    inspire_response = requests.get(inspire_url)
+    print(f"> Search paper {semantic_paper.arxiv_id} in Inpsire HEP...")
+    inspire_paper = search_in_inspire(semantic_paper.arxiv_id)
+    inspire_paper.corpus_id = semantic_paper.corpus_id
+    inspire_paper.arxiv_id = semantic_paper.arxiv_id
+    inspire_paper.semantic_link = semantic_paper.semantic_link
+    print(inspire_paper)
 
-    if inspire_response.status_code != 404:
-        inspire_content = json.loads(inspire_response.text).get("metadata")
-
-        # update paper info
-        paper.title = inspire_content["titles"][0]["title"]
-        paper.title = paper.title if not review else f"✧ {paper.title}"
-        if "collaborations" in inspire_content:
-            paper.authors = [inspire_content["collaborations"][0]["value"] + " Collaboration"]
-        else:
-            paper.authors = []
-            for author in inspire_content["authors"][:10]:
-                for i in author["ids"]:
-                    if i["schema"] == "INSPIRE BAI":
-                        paper.authors.append(i["value"][:-2])
-
-        paper.citations = inspire_content["citation_count"]
-
-        match inspire_content["document_type"][0]:
-            case "article":
-                if "publication_info" in inspire_content:
-                    paper.published = inspire_content["publication_info"][0].get("journal_title", "Unpublished")
-                else:
-                    paper.published = "Unpublished"
-            case "conference paper":
-                if "publication_info" in inspire_content:
-                    for i in inspire_content["publication_info"]:
-                        if "cnum" in i:
-                            conference_url = i["conference_record"]["$ref"]
-                            conference_response = requests.get(conference_url)
-                            paper.published = json.loads(conference_response.text)["metadata"][
-                                "acronyms"
-                            ][0]
-                else:
-                    paper.published = "Unpublished"
-            case _:
-                paper.published = "Unknown"
-
-        paper.source = "Inspire"
-        paper.inspire_link = (
-            f"https://inspirehep.net/literature/{inspire_content['control_number']}"
-        )
-
-        bibtex_url = f"{inspire_url}?format=bibtex"
-        bibtex_response = requests.get(bibtex_url)
-        paper.bibtex = bibtex_response.text[:-1]  # remove the final \n
-    else:
-        print(f"! Couldn't find paper {paper.corpus_id} arxiv_id on Inspire HEP")
-        print(f"! Use Semantic source insead")
-
-    if dry_run:
-        print(paper)
-    else:
-
-        # notion
-        # ---------------------------------------------------------------------------- #
-        paper_library_database_id = "841535b763e249579001f81df2a72369"
-        professors_database_id = "70c49e69591c4cae8dee77c55b5237b2"
-        token = "secret_wXJ2PM5Ff2xAx6vpoxTMGOBfiCzjraD3Oco0qhzuMcY"
-
-        properties = {
-            # base info
-            "Title": to_property("title", paper.title),
-            "Authors": to_relation(paper.authors, professors_database_id, token, source),
-            "Citations": to_property("number", paper.citations),
-            "Published": to_property("select", paper.published),
-            # "TLDR": to_property("rich_text", paper.tldr),
-            "Source": to_property("select", paper.source),
-            "Topic": to_property("select", paper.topic),
-            "Corpus ID": to_property("rich_text", paper.corpus_id),
-            "Semantic Link": to_property("url", paper.semantic_link),
-        }
-        if inspire_response.status_code != 404:
-            properties.update(
-                {
-                    "Arxiv ID": to_property("rich_text", paper.arxiv_id),
-                    "Inspire Link": to_property("url", paper.inspire_link),
-                    "Bibtex": to_property("rich_text", paper.bibtex),
-                }
-            )
-
-        notion_url = "https://api.notion.com/v1/pages"
-        payload = {
-            "parent": {
-                "type": "database_id",
-                "database_id": paper_library_database_id,
-            },
-            "properties": properties,
-        }
-        headers = {
-            "accept": "application/json",
-            "Notion-Version": "2022-06-28",
-            "content-type": "application/json",
-            "authorization": f"Bearer {token}",
-        }
-
-        notion_response = requests.post(notion_url, json=payload, headers=headers)
-
-        match notion_response.status_code:
-            case 200:
-                print(f"✓ Succesfully added paper {paper.corpus_id}")
-                print(paper)
-            case _:
-                print(f"✘ Something wrong with paper {paper.corpus_id}")
-                print(notion_response.text)
+    print(f"> Add paper to Notion...")
+    add_to_notion(inspire_paper, topic)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
--- utils.py
+++ utils.py
@@ -2,101 +2,189 @@ import json
 from dataclasses import dataclass, field
 
 import requests
-from rich import print
 
 # ---------------------------------------------------------------------------- #
-def query_database(database_id, token, source, cache=False):
-    database_file = f"database-{database_id}.json"
-    match source:
-        case "local":
-            print(f"-> Read local database")
-            with open(database_file, "r") as f:
-                database = json.load(f)
-        case "remote":
-            print(f"-> Fetch remote database")
-            url = f"https://api.notion.com/v1/databases/{database_id}/query"
+@dataclass
+class Paper:
+    title: str = ""
+    authors: list[str] = field(default_factory=list)
+    published: str = ""
+    citations: int = 0
 
-            payload = {
-                "page_size": 100,
-            }
-            headers = {
-                "accept": "application/json",
-                "Notion-Version": "2022-06-28",
-                "content-type": "application/json",
-                "authorization": f"Bearer {token}",
-            }
-            response = requests.post(url, json=payload, headers=headers)
-            database = json.loads(response.text)
+    corpus_id: str = ""
+    arxiv_id: str = ""
+    semantic_link: str = ""
+    inspire_link: str = ""
 
-            # print(f"Saved in {database_file}")
+    bibtex: str = ""
 
-        case _:
-            raise NameError("Choose source between local and remote!")
+
+def search_in_semantic(corpus_id: str):
+    api = "https://api.semanticscholar.org/graph/v1/paper"
+    fields = "title,authors,citationCount,journal,externalIds,url"
+    url = f"{api}/CorpusID:{corpus_id}?fields={fields}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(response.text)
+        raise LookupError(f"✘ Something wrong with paper {corpus_id}")
+
+    # ------------------------------------------------------------------------ #
+    metadata = json.loads(response.text)
+    # with open(f"semantic_{corpus_id}.json", "w") as f:
+    #     json.dump(metadata, f, indent=4)
+
+    paper = Paper()
+    paper.title = metadata["title"]
+    paper.authors = [author["name"] for author in metadata["authors"][:10]]
+    paper.published = metadata["journal"]["name"] if metadata["journal"] else "Unpublished"
+    paper.citations = metadata["citationCount"]
+    paper.corpus_id = corpus_id
+    paper.arxiv_id = metadata["externalIds"].get("ArXiv", "")
+    paper.semantic_link = metadata["url"]
+
+    return paper
+
+
+def search_in_inspire(arxiv_id: str):
+    api = "https://inspirehep.net/api"
+    url = f"{api}/arxiv/{arxiv_id}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(response.text)
+        raise LookupError(f"✘ Something wrong with paper {arxiv_id}")
+
+    # ------------------------------------------------------------------------ #
+    content = json.loads(response.text)
+    bibtex_url = content["links"]["bibtex"]
+    metadata = content["metadata"]
+    # with open(f"inspire_{arxiv_id}.json", "w") as f:
+    #     json.dump(metadata, f, indent=4)
+
+    paper = Paper()
+    paper.title = metadata["titles"][0]["title"]
+    # author
+    if "collaborations" in metadata:
+        paper.authors = [metadata["collaborations"][0]["value"] + " Collaboration"]
+    else:
+        for author in metadata["authors"][:10]:
+            for i in author["ids"]:
+                if i["schema"] == "INSPIRE BAI":
+                    paper.authors.append(i["value"][:-2])
+    # journal or conference
+    if metadata["document_type"][0] == "article":
+        paper.published = metadata.get("publication_info", [{}])[0].get(
+            "journal_title", "Unpublished"
+        )
+    elif metadata["document_type"][0] == "conference paper":
+        for i in metadata["publication_info"]:
+            if "cnum" in i:
+                conference_url = i["conference_record"]["$ref"]
+                conference_response = requests.get(conference_url)
+                paper.published = json.loads(conference_response.text)["metadata"]["acronyms"][0]
+    else:
+        paper.published = "Unknown"
+    # citations
+    paper.citations = metadata["citation_count"]
+    # link
+    paper.inspire_link = f"https://inspirehep.net/literature/{metadata['control_number']}"
+    # bibtex
+    bibtex_response = requests.get(bibtex_url)
+    paper.bibtex = bibtex_response.text[:-1]  # remove the final \n
+
+    return paper
+
+
+def query_a_database(database_id, token):
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+
+    payload = {"page_size": 100}
+    headers = {
+        "accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}",
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code != 200:
+        print(response.text)
+        raise LookupError(f"✘ Something wrong")
+
+    # ------------------------------------------------------------------------ #
+    database = json.loads(response.text)
+    # with open(f"database_{database_id}.json", "w") as f:
+    #     json.dump(database, f, indent=4)
 
     return database
 
 
-# ---------------------------------------------------------------------------- #
-def to_property(property, value):
+def create_a_page(properties, database_id, token):
+    url = "https://api.notion.com/v1/pages"
+    payload = {
+        "parent": {
+            "type": "database_id",
+            "database_id": database_id,
+        },
+        "properties": properties,
+    }
+    headers = {
+        "accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}",
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 200:
+        print(f"✓ Succesfully created in {database_id}")
+    else:
+        print(response.text)
+        raise LookupError(f"✘ Something wrong")
+
+
+def to_property(property, value, **kwargs):
     match property:
         case "title" | "rich_text":
             return {property: [{"text": {"content": value}}]}
 
         case "number" | "url":
-
             return {property: value}
 
         case "select" | "status":
             return {property: {"name": value}}
 
         case "multi_select":
-            return {"multi_select": [{"name": str(i)} for i in value]}
+            return {property: [{"name": str(i)} for i in value]}
+
+        case "relation":
+            database = query_a_database(kwargs["database_id"], kwargs["token"])
+            relation = {}
+            for i in database["results"]:
+                _name = i["properties"]["Name"]["title"][0]["plain_text"]
+                _id = i["id"]
+                relation[_name] = _id
+            return {property: [{"id": relation[i]} for i in value if i in relation]}
 
 
-# ---------------------------------------------------------------------------- #
-def to_relation(
-    values: list[str],
-    database_id: str,
-    token: str,
-    source: str,
-):
-    match source:
-        case "local":
-            with open(f"database-{database_id}.json", "r") as f:
-                database = json.load(f)
+def add_to_notion(paper: Paper, topic: str = "Unknown"):
+    literatures_database_id = "841535b763e249579001f81df2a72369"
+    professors_database_id = "70c49e69591c4cae8dee77c55b5237b2"
+    token = "secret_wXJ2PM5Ff2xAx6vpoxTMGOBfiCzjraD3Oco0qhzuMcY"
 
-        case "remote":
-            database = query_database(database_id, token, source="remote")
+    properties = {
+        "Title": to_property("title", paper.title),
+        "Authors": to_property(
+            "relation",
+            paper.authors,
+            database_id=professors_database_id,
+            token=token,
+        ),
+        "Citations": to_property("number", paper.citations),
+        "Published": to_property("select", paper.published),
+        "Topic": to_property("select", topic),
+        "Corpus ID": to_property("rich_text", paper.corpus_id),
+        "Arxiv ID": to_property("rich_text", paper.arxiv_id),
+        "Semantic Link": to_property("url", paper.semantic_link),
+        "Inspire Link": to_property("url", paper.inspire_link),
+        "Bibtex": to_property("rich_text", paper.bibtex),
+    }
 
-        case _:
-            raise NameError("Choose source between local and remote!")
-
-    relation = {}
-    for i in database["results"]:
-        _name = i["properties"]["Name"]["title"][0]["plain_text"]
-        _id = i["id"]
-        relation[_name] = _id
-
-    return {"relation": [{"id": relation[i]} for i in values if i in relation]}
-
-
-# ---------------------------------------------------------------------------- #
-@dataclass
-class Paper:
-    # base information
-    title: str = ""
-    authors: list[str] = field(default_factory=list)
-    citations: int = 0
-    published: str = ""
-    tldr: str = ""
-    source: str = ""
-
-    # ids & links
-    corpus_id: str = ""
-    arxiv_id: str = ""
-    semantic_link: str = ""
-    inspire_link: str = ""
-
-    # other
-    bibtex: str = ""
-    topic: str = ""
+    create_a_page(properties, literatures_database_id, token)
