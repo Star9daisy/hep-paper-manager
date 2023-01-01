7362ae645074ea08eff8938e68d788c248161a77
Star9daisy <star9daisy@outlook.com>
2023-01-01 21:15:12 +0800

    feat: Add gum choose to select a topic

--- paper_adder.py
+++ paper_adder.py
@@ -1,6 +1,7 @@
 import typer
 from rich import print
 from utils import search_in_semantic, search_in_inspire, add_to_notion
+import subprocess, shlex
 
 
 # test data
@@ -13,10 +14,18 @@ app = typer.Typer()
 
 
 @app.command()
-def main(corpus_id: str, topic: str = "Unknown"):
+def main():
+    corpus_id = subprocess.run(
+        shlex.split("gum input --placeholder 'Enter a corpus ID'"),
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    corpus_id = corpus_id.stdout.strip()
+
     print(f"> Search paper {corpus_id} in Semantic Scholar...")
     semantic_paper = search_in_semantic(corpus_id)
     print(semantic_paper)
+    print()
 
     print(f"> Search paper {semantic_paper.arxiv_id} in Inpsire HEP...")
     inspire_paper = search_in_inspire(semantic_paper.arxiv_id)
@@ -24,8 +33,31 @@ def main(corpus_id: str, topic: str = "Unknown"):
     inspire_paper.arxiv_id = semantic_paper.arxiv_id
     inspire_paper.semantic_link = semantic_paper.semantic_link
     print(inspire_paper)
+    print()
 
-    print(f"> Add paper to Notion...")
+    print("> Choose a topic, press ctrl+c to jump over")
+    topics = ' '.join([
+        "Anomaly",
+        "Architecture",
+        "Generative",
+        "Interpretability",
+        "Measurement",
+        "Reconstruction",
+        "Tagging",
+        "Observable",
+        "Decorrelation",
+        "Uncertainty",
+        "PhaseSpace",
+        "zOther",
+    ])
+    topic = subprocess.run(
+        shlex.split(f"gum choose {topics}"),
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    topic = topic.stdout.strip()
+    topic = topic if topic else "Unknown"
+    print(f"> Add paper({topic}) to Notion...")
     add_to_notion(inspire_paper, topic)
 
 
