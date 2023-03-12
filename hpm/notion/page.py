d5653f9d59c22f362d242519e14e4997a5e15369
Star9daisy <star9daisy@outlook.com>
2023-03-12 21:33:48 +0800

    feat: add update_page

--- hpm/notion/page.py
+++ hpm/notion/page.py
@@ -16,3 +16,19 @@ def create_page(token: str, parent: dict, properties: dict) -> Response:
     }
 
     return requests.post(url, json=payload, headers=headers)
+
+
+def update_page(token: str, page_id: str, properties: dict) -> Response:
+    url = f"https://api.notion.com/v1/pages/{page_id}"
+
+    payload = {
+        "properties": properties,
+    }
+    headers = {
+        "accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}",
+    }
+
+    return requests.patch(url, json=payload, headers=headers)
