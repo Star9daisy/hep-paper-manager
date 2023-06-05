0709299afb3d5457ca292e74559f42924012fe63
Star9daisy <star9daisy@outlook.com>
2023-06-05 20:46:19 +0800

    feat: Add cach dir in init file

--- hpm/__init__.py
+++ hpm/__init__.py
@@ -1,2 +1,9 @@
+from pathlib import Path
+
 __app_name__ = "HEP Paper Manager"
 __app_version__ = "0.1.0"
+
+CACHE_DIR = Path.home() / ".cache" / "hpm"
+CACHE_DIR.mkdir(parents=True, exist_ok=True)
+CACHED_PAPERS_DIR = CACHE_DIR / "cached_papers"
+CACHED_AUTHORS_DIR = CACHE_DIR / "cached_authors"
