fad68481f948ec707cdd8dea7d53f456f4bdc4ad
Star9daisy <star9daisy@outlook.com>
2023-06-12 12:40:08 +0800

    test: Update test of page-related functions

--- tests/test_client_page_functions.py
+++ tests/test_client_page_functions.py
@@ -9,86 +9,75 @@ from hpm.notion.properties import *
 load_dotenv(".env")
 token = os.getenv("TOKEN")
 client = Client(token)
+parent_id = "19ef8e6eab3542aa91a2f4aa54623479"
+page_id0 = "d18e78e482e94a3995c18e1932ae9fb8"
+page_id1 = "48cb3c1ee9664680884dd7bfa05913a3"
 
 
 def test_retrieve_page():
-    expected_01 = Page(
-        id="633144dd78bf4fe6800e83a530c3d87e",
-        parent_id="480d39ea2a6d49d3a6e9af7b2aed8152",
-        properties=[
-            RichText(name="ID", value="01"),
-            Select(name="Journal", value="JournalA"),
-            Number(name="Citations", value=90),
-            Title(name="Title", value="Paper with title 1"),
-        ],
-        url="https://www.notion.so/Paper-with-title-1-633144dd78bf4fe6800e83a530c3d87e",
+    expected0 = Page(
+        parent_id="19ef8e6eab3542aa91a2f4aa54623479",
+        properties={
+            "p_url": URL(value=None),
+            "p_select": Select(value=None),
+            "p_number": Number(value=None),
+            "p_relation": Relation(value=[]),
+            "p_multi_select": MultiSelect(value=[]),
+            "p_rich_text": RichText(value=""),
+            "p_title": Title(value=""),
+        },
+        id="d18e78e482e94a3995c18e1932ae9fb8",
+        url="https://www.notion.so/d18e78e482e94a3995c18e1932ae9fb8",
     )
-    page_01 = client.retrieve_page("633144dd78bf4fe6800e83a530c3d87e")
-    assert page_01 == expected_01
-
-    expected_02 = Page(
-        id="eda17f339aa24341b6ab07bdf0e55b24",
-        parent_id="480d39ea2a6d49d3a6e9af7b2aed8152",
-        properties=[
-            RichText(name="ID", value="02"),
-            Select(name="Journal", value="JournalB"),
-            Number(name="Citations", value=20),
-            Title(name="Title", value="Paper with title 2"),
-        ],
-        url="https://www.notion.so/Paper-with-title-2-eda17f339aa24341b6ab07bdf0e55b24",
+    expected1 = Page(
+        parent_id="19ef8e6eab3542aa91a2f4aa54623479",
+        properties={
+            "p_url": URL(value="https://developers.notion.com/reference/property-object"),
+            "p_select": Select(value="topic1"),
+            "p_number": Number(value=1),
+            "p_relation": Relation(
+                value=[
+                    "633144dd78bf4fe6800e83a530c3d87e",
+                    "eda17f339aa24341b6ab07bdf0e55b24",
+                    "3f4ce0fddde24c8fb3012905170c2034",
+                ]
+            ),
+            "p_multi_select": MultiSelect(value=["tag1", "tag2"]),
+            "p_rich_text": RichText(value="This is a rich text property"),
+            "p_title": Title(value="Item1"),
+        },
+        id="48cb3c1ee9664680884dd7bfa05913a3",
+        url="https://www.notion.so/Item1-48cb3c1ee9664680884dd7bfa05913a3",
     )
-    page_02 = client.retrieve_page("eda17f339aa24341b6ab07bdf0e55b24")
-    assert page_02 == expected_02
-
-    expected_03 = Page(
-        id="3f4ce0fddde24c8fb3012905170c2034",
-        parent_id="480d39ea2a6d49d3a6e9af7b2aed8152",
-        properties=[
-            RichText(name="ID", value="03"),
-            Select(name="Journal", value="JournalC"),
-            Number(name="Citations", value=5),
-            Title(name="Title", value="Paper with title 3"),
-        ],
-        url="https://www.notion.so/Paper-with-title-3-3f4ce0fddde24c8fb3012905170c2034",
-    )
-    page_03 = client.retrieve_page("3f4ce0fddde24c8fb3012905170c2034")
-    assert page_03 == expected_03
+    page0 = Page.from_response(client.retrieve_page(page_id0))
+    page1 = Page.from_response(client.retrieve_page(page_id1))
+    assert page0 == expected0
+    assert page1 == expected1
 
 
 def test_update_page():
-    page_01 = client.retrieve_page("633144dd78bf4fe6800e83a530c3d87e")
-    page_01.set_property("ID", "1234")
+    page0 = Page.from_response(client.retrieve_page(page_id0))
+    page0.properties["p_title"] = Title(value="New title")
 
-    new_page_01 = client.update_page(page_01)
-    assert new_page_01.get_property("ID") == "1234"
+    new_page0 = Page.from_response(client.update_page(page0))
+    assert new_page0.properties["p_title"] == Title(value="New title")
 
-    new_page_01.set_property("ID", "01")
-    old_page_01 = client.update_page(new_page_01)
-    assert old_page_01 == new_page_01
+    new_page0.properties["p_title"] = Title()
+    old_page0 = Page.from_response(client.update_page(new_page0))
+    assert old_page0.properties["p_title"] == Title()
 
 
 def test_create_archive_restore_page():
-    page = Page(
-        id=None,
-        parent_id="480d39ea2a6d49d3a6e9af7b2aed8152",
-        properties=[
-            RichText(name="ID", value="04"),
-            Number(name="Citations", value=0),
-            Title(name="Title", value="Paper with title 4"),
-        ],
-        url=None,
-    )
+    page0 = Page(parent_id=parent_id)
 
-    new_page = client.create_page(page, page.parent_id)
-    assert new_page.get_property("ID") == "04"
-    assert new_page.get_property("Citations") == 0
-    assert new_page.get_property("Title") == "Paper with title 4"
-    assert new_page.get_property("Journal") == None
+    new_page0 = Page.from_response(client.create_page(page0))
+    old_page0 = Page.from_response(client.retrieve_page(page_id0))
+    assert new_page0.properties == old_page0.properties
 
-    archived_page = client.archive_page(new_page)
-    assert archived_page == new_page
+    archived_page0 = Page.from_response(client.archive_page(new_page0))
+    assert archived_page0 == new_page0
 
-    restored_page = client.restore_page(archived_page)
-    assert restored_page == archived_page
+    restored_page0 = Page.from_response(client.restore_page(archived_page0))
+    assert restored_page0 == archived_page0
 
-    client.archive_page(restored_page)
+    client.archive_page(restored_page0)
