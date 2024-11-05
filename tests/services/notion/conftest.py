4321530ccf25d5a83e667398bcb151697d3119e7
Star9daisy <star9daisy@gmail.com>
2024-11-05 14:17:11 +0000

    tests: Add page and database ids as fixtures

---
+++ tests/services/notion/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture
+def TEST_PAGE_ID():
+    return "1271444c50e4808ba7bbc9d0d431c208"
+
+
+@pytest.fixture
+def TEST_DATABASE_ID():
+    return "1261444c50e4808e83cde7c2fe05ded8"
+
+
+@pytest.fixture
+def TEST_PAPERS_DATABASE_ID():
+    return "12d1444c50e4804cb93be067530fa954"
